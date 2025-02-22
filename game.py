dfb4659b09f9b15ff3c4be5694c0e8da28f37fc5
knyaz <saba2006.diasamidze@gmail.com>
2025-02-22 23:09:58 +0400

    idk

--- game.py
+++ game.py
@@ -1,4 +1,6 @@
 import pyray as pr
+from entity import Entity
+from resource_type import ResourceType
 
 class Game:
 	def __init__(self, screen_width, screen_height):
@@ -8,11 +10,33 @@ class Game:
 		self.meteors = []
 		self.shots = []
 		#variables.
+		self.player = Entity()
 		self.screen_width = screen_width
 		self.screen_height = screen_height
 
 	def startup(self):
-		pass
+		pr.init_audio_device()
+
+		image = pr.load_image("assets/sprite-0004.png")
+		self.resources[ResourceType.TEXTURE_METEOR_SMALL] = pr.load_texture_from_image(image)
+		pr.unload_image(image)
+
+		image = pr.load_image("assets/sprite-0003.png")
+		self.resources[ResourceType.TEXTURE_METEOR_MEDIUM] = pr.load_texture_from_image(image)
+		pr.unload_image(image)
+
+		image = pr.load_image("assets/sprite-0002.png")
+		self.resources[ResourceType.TEXTURE_METEOR_LARGE] = pr.load_texture_from_image(image)
+		pr.unload_image(image)
+
+		image = pr.load_image("assets/sprite-0001.png")
+		self.resources[ResourceType.TEXTURE_PLAYER] = pr.load_texture_from_image(image)
+		pr.unload_image(image)
+
+		self.resources[ResourceType.SOUND_LAZER_SHOOT] = pr.load_sound("assets/audio-0001.mp3")
+		self.resources[ResourceType.SOUND_LAZER_EXPLOSION] = pr.load_sound("assets/audio-0002.mp3")
+
+		self.reset()
 
 	def update(self):
 		pass
@@ -21,4 +45,26 @@ class Game:
 		pass
 
 	def shutdown(self):
-		pass
+		pr.unload_texture(self.resources[ResourceType.TEXTURE_METEOR_SMALL])
+		pr.unload_texture(self.resources[ResourceType.TEXTURE_METEOR_MEDIUM])
+		pr.unload_texture(self.resources[ResourceType.TEXTURE_METEOR_LARGE])
+		pr.unload_texture(self.resources[ResourceType.TEXTURE_PLAYER])
+
+		pr.unload_sound(self.resources[ResourceType.SOUND_LAZER_SHOOT])
+		pr.unload_sound(self.resources[ResourceType.SOUND_LAZER_EXPLOSION])
+		pr.close_audio_device()
+
+	def reset(self):
+		self.shots.clear()
+		self.meteors.clear()
+
+		#player settings when game is reset.
+		self.player.heading = 0.00
+		self.player.acceleration = 0.00
+		self.player.active = true
+		self.player.speed = pr.Vector2(0, 0)
+		self.player.position = pr.Vector2(self.screen_width // 2, self.screen_height // 2)
+
+
+
+
--- main.py
+++ main.py
@@ -1,15 +1,28 @@
 import pyray as pr
+from game import Game
+
 
 if __name__ == '__main__':
 	screen_width = 1280
 	screen_height = 720
+	current_game = Game(screen_width, screen_height)
 
 	pr.init_window(screen_width, screen_height, "asteroids")
+
 	pr.set_target_fps(60)
 
+	current_game.startup()
+
 	while not pr.window_should_close():
+		current_game.update()
+
 		pr.begin_drawing()
+
+		current_game.render()
+
 		pr.clear_background(pr.BLACK)
+
 		pr.end_drawing()
 
 	pr.close_window()
+	current_game.shutdown()
